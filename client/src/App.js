4e534bb42499983ddd3d5dfc6578f24b3d91dde8
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-03-13 17:18:10 -0500

    added auth context and logout function, base routes for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,62 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { UserContext, getUser, logoutUser } from "./utils/auth";
+import { auth } from "./config/firebase";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Home } from "./components/Home";
 import "./App.css";
-import axios from "axios";
 
 const App = () => {
-  const [message, setMessage] = useState("");
-
-  const getMessage = async () => {
-    const res = await axios.get(`/api/`);
-    console.log(res.data);
-    setMessage(res.data);
-  };
+  const [user, setUser] = useState(auth.currentUser); // TODO: Setup Context or Redux Store
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    (async () => {
-      await getMessage();
-    })();
+    auth.onAuthStateChanged(async (user) => {
+      setUser(user);
+      if (user) {
+        setLoading(true);
+        const userInfo = await getUser(user.uid);
+        console.log("User Logged In", userInfo);
+        setUserData(userInfo);
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    });
   }, []);
+
+  const handleLogout = async () => {
+    const { code, message } = (await logoutUser()) || {};
+    if (code) {
+      console.log(`${code}: ${message}`);
+    }
+    setUserData(null);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-      </header>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div className="home">
+          <BrowserRouter>
+            <UserContext.Provider value={{ user, userData }}>
+              <Switch>
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+                <Route path="/" component={Home} />
+              </Switch>
+            </UserContext.Provider>
+          </BrowserRouter>
+          {user && (
+            <button onClick={handleLogout} color="inherit">
+              Log Out
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
