572e71a5fc5f26be3ca944845c18edfcc5756bd1
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-03-13 17:19:44 -0500

    added firebase auth functions for adding users, loggin in, logging out, and getting user info

---
+++ client/src/utils/auth.js
@@ -0,0 +1,72 @@
+import { createContext } from "react";
+import { auth, db } from "../config/firebase";
+import "firebase/firestore";
+
+/* User Context Object */
+export const UserContext = createContext({
+  user: null,
+  userData: null,
+});
+
+/**
+ * @desc Register user
+ * @return User Object
+ * @param {*} user
+ */
+export const registerUser = async (userData) => {
+  return auth
+    .createUserWithEmailAndPassword(userData.email, userData.password)
+    .then(({ user }) => {
+      const { password, ...newUserInfo } = userData; // do not directly store passwords
+
+      db.collection("users").doc(user.uid).set(newUserInfo); // TODO: Move to backend endpoint and link with service function
+      return { user: user };
+    })
+    .catch((err) => {
+      return { error: err };
+    });
+};
+
+/**
+ * @desc Login User
+ * @return  User Object
+ * @param {*} user
+ */
+export const loginUser = async ({ email, password }) => {
+  return auth
+    .signInWithEmailAndPassword(email, password)
+    .then(({ user }) => {
+      return { user: user };
+    })
+    .catch((err) => {
+      return { error: err };
+    });
+};
+
+/**
+ * @desc Logout User
+ * @return Error || Nothing
+ * @param {*} user
+ */
+export const logoutUser = async () => {
+  return auth.signOut().catch((err) => {
+    return err;
+  });
+};
+
+// TODO: Move to backend endpoint and link with service function
+export const getUser = async (uid) => {
+  const userRef = db.collection("users").doc(uid);
+  try {
+    const doc = await userRef.get();
+    if (doc.exists) {
+      return doc.data();
+    } else {
+      console.log(
+        "Error. This user breaches the plane of existence. User DNE."
+      );
+    }
+  } catch (error) {
+    console.log("error getting document in getUser");
+  }
+};
