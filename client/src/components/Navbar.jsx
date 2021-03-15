aa7e6265eec770462f9028a8e54f7b9c0af6b09c
xshi0603 <xshi603@gmail.com>
2021-03-14 20:02:41 -0400

    refacted handler code to be more modular

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
+import NavbarLink from "./NavbarLink";
 
 // TODO: Handle Navigation B/t components better
 export const Navbar = () => {
-  const history = useHistory();
+  // const history = useHistory();
 
-  const handleLogin = () => {
-    history.push("/login");
-  };
+  // const handleHome = () => {
+  //   history.push("/");
+  // };
 
-  const handleRegister = () => {
-    history.push("/register");
-  };
+  // const handleLogin = () => {
+  //   history.push("/login");
+  // };
 
-  const handleHome = () => {
-    history.push("/");
-  };
+  // const handleRegister = () => {
+  //   history.push("/register");
+  // };
 
   return (
     <nav>
-      <button onClick={handleHome}> Home </button>
+      <NavbarLink link="/" display="Home" />
+      <NavbarLink link="/login" display="Login" />
+      <NavbarLink link="/register" display="Register" />
+      {/* <button onClick={handleHome}> Home </button>
       <button onClick={handleLogin}> Login </button>
-      <button onClick={handleRegister}> Register </button>
+      <button onClick={handleRegister}> Register </button> */}
     </nav>
   );
 };
--- client/src/components/NavbarLink.jsx
+++ client/src/components/NavbarLink.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 
 const NavbarLink = ({link, display}) => {
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push(link);
+    }
+
+    // const handleHome = () => {
+    //     history.push("/");
+    // };
+
     return (
+        // <li className="nav-item">
+        //     <a className = "nav-link" href={link}>{display}</a>
+        // </li> 
         <li className="nav-item">
-            <a className = "nav-link" href={link}>{display}</a>
+            <a className = "nav-link" onClick={handleClick}>{display}</a>
         </li> 
     )
 }
