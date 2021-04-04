1ecd727d6676e91ff68711024bceaa4b6e68c68e
Darren Liang <liangdarren2@gmail.com>
2021-04-04 17:45:31 -0400

    set up groundwork for search functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../utils/auth";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { Organization } from "../components/Organization";
 import "../styles/Search.css"
-import { Container, Row } from "react-bootstrap"
-
-import { InputGroup, FormControl, Button } from "react-bootstrap";
+import { InputGroup, FormControl, Button, Container, Row } from "react-bootstrap";
 
 export const Search = () => {
+    const history = useHistory();
     const [organizations, setOrganization] = useState(""); /* REST API enpoint */
+    const [searchData, setSearchData] = useState("");
+    const [hasSearched, setHasSearched] = useState(false);
+
     const getOrganization = async () => {
         const res = await axios.get(`/api/cn/organizations`);
         setOrganization(res.data);
-        console.log(res.data);
     };
+
+    const getSearchResults = async () => {
+        const res = await axios.get('/api/cn/organizations', {
+            body: {
+                search: searchData
+            }
+        })
+        setOrganization(res.data);
+    }
 
     useEffect(() => {
         (async () => {
@@ -21,31 +32,51 @@ export const Search = () => {
         })();
     }, []);
 
-    const generateOrganization = () => {
+      /* Search */
+    const handleSubmit = async (e) => {
+        console.log(searchData);
+        setHasSearched(true);
+        getSearchResults();
+    };
+
+    const handleChange = (e) => {
+        setSearchData(e.target.value);
+    };
+
+    const generateOrganizations = () => {
         if (organizations.length === 0) {
-          return "No Results";
+            return "No Results";
         }
         else {
-          return organizations.map(organization => {
-              return <Organization key={organization.ein} name={organization.charityName} />
-          });
+            return organizations.map(organization => {
+            return <Organization key={organization.ein} name={organization.charityName} />
+            });
         }
-      }
+    }
 
     return ( 
         <Container style={{paddingTop: 60, paddingBottom: 83, }}>
-          <InputGroup className="mx-auto" style={{width:"50%", paddingTop: 60, marginTop: "5%",}}>
+          <InputGroup onChange={handleChange} className="mx-auto" style={{width:"50%", paddingTop: 60, marginTop: "5%",}}>
             <FormControl 
                 placeholder="Search for a Charity"
                 aria-label="Search for a Charity"
                 aria-describedby="basic-addon2"
             />
             <InputGroup.Append>
-                <Button variant="outline-secondary">Search</Button>
+                <Button onClick={handleSubmit} noValidate variant="outline-secondary" type="submit">Search</Button>
             </InputGroup.Append>
         </InputGroup>
-            Recommendations 
-            <Row>{generateOrganization()}</Row>
+        {!hasSearched ?
+            <>
+            Recommendations
+            </>
+            :
+            <>
+            Results
+            </>
+        }
+        <Row>{generateOrganizations()}</Row>
+    
         </Container>
     )
 }
