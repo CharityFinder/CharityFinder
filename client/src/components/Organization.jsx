93e05b093aa631f3b19a9bb1b5f1526013f72ade
Darren Liang <liangdarren2@gmail.com>
2021-04-07 21:49:12 -0400

    more mobile styling

--- client/src/components/Organization.jsx
+++ client/src/components/Organization.jsx
@@ -4,12 +4,11 @@ import "../styles/Search.css"
 
 export const Organization = ({name}) => {
     return (
-        <Card className="column" border="warning" style={{ width: 'auto', margin: 5, }}>
+        <Card className="column" border="warning" style={{margin: 5, }}>
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk
-            of the card's content.
+
           </Card.Text>
         </Card.Body>
       </Card>
