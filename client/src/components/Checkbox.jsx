b1ecbfb5d50d2c7c8fc6809f66085a76dd8e2bef
xshi0603 <xshi603@gmail.com>
2021-04-16 02:19:49 -0400

    fixed checkbox problem on surveys page

--- client/src/components/Checkbox.jsx
+++ client/src/components/Checkbox.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { Form } from "react-bootstrap";
 
 export const Checkbox = ({inputLabel, inputOnChange}) => {
   return (
-    <Form.Check className="mx-auto" type="checkbox" id='default-checkbox' style={{textAlign: "left"}}>
+    <Form.Check className="mx-auto" type="checkbox" id={'default-checkbox-' + inputLabel} style={{textAlign: "left"}} >
       <Form.Check.Input  type="checkbox" name={inputLabel} onChange={inputOnChange} />
       <Form.Check.Label> {inputLabel} </Form.Check.Label>
     </Form.Check>
