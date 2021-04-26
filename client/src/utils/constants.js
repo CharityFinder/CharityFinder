9f179d1667d3fb85383cb4560bb96cdc8c55699d
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-04-26 12:12:21 -0400

    added cause areas to array object for survey display options integration

---
+++ client/src/utils/constants.js
@@ -0,0 +1,76 @@
+/* Cause Areas In CharityNavigator */
+export const causes = [
+  /* ANIMALS */
+  { causeID: 1, causeName: "Wildlife Conservation" },
+  { causeID: 2, causeName: "Animal Rights, Welfare, and Services" },
+  { causeID: 32, causeName: "Zoos and Aquariums" },
+
+  /* THE ARTS */
+  { causeID: 3, causeName: "Museums" },
+  { causeID: 4, causeName: "Performing Arts" },
+  { causeID: 5, causeName: "Public Broadcasting and Media" },
+  {
+    causeID: 6,
+    causeName: "Libraries, Historical Societies and Landmark Preservation",
+  },
+  /* NATURE */
+  { causeID: 10, causeName: "Botanical Gardens, Parks, and Nature Centers" },
+  { causeID: 11, causeName: "Environmental Protection and Conservation" },
+
+  /* MEDICINE */
+  { causeID: 12, causeName: "Treatment and Prevention Services" },
+  { causeID: 13, causeName: "Diseases, Disorders, and Disciplines" },
+  { causeID: 14, causeName: "Medical Research" },
+  { causeID: 34, causeName: "Patient and Family Support" },
+
+  /* HUMANITARIAN */
+  { causeID: 30, causeName: "Humanitarian Relief Supplies" },
+  { causeID: 15, causeName: "Multipurpose Human Service Organizations" },
+
+  /* CHILDREN */
+  { causeID: 16, causeName: "Youth Development, Shelter, and Crisis Services" },
+  { causeID: 17, causeName: "Children's and Family Services" },
+  { causeID: 36, causeName: "Early Childhood Programs and Services" },
+
+  /* HUNGER */
+  {
+    causeID: 18,
+    causeName: "Food Banks, Food Pantries, and Food Distribution",
+  },
+
+  /* GLOBAL INTIATIVES */
+  { causeID: 19, causeName: "International Peace, Security, and Affairs" },
+
+  /* DEVELOPMENT AND RELIEF SERVICES */
+  { causeID: 20, causeName: "Development and Relief Services" },
+
+  /* EDUCATION */
+  { causeID: 21, causeName: "Advocacy and Education" },
+  { causeID: 37, causeName: "Scholarship and Financial Support" },
+  { causeID: 38, causeName: "Special Education" },
+  { causeID: 39, causeName: "Adult Education Programs and Services" },
+  { causeID: 40, causeName: "Youth Education Programs and Services" },
+  { causeID: 41, causeName: "Education Policy and Reform" },
+
+  /* SOCIAL REFORM */
+  { causeID: 22, causeName: "Community Foundations" },
+  { causeID: 24, causeName: "Social and Public Policy Research" },
+
+  /* RELIGIOUS */
+  { causeID: 25, causeName: "Religious Media and Broadcasting" },
+  { causeID: 26, causeName: "Religious Activities" },
+
+  /* HOMELESSNESS */
+  { causeID: 27, causeName: "Housing and Neighborhood Development" },
+  { causeID: 28, causeName: "Homeless Services" },
+  { causeID: 29, causeName: "Social Services" },
+
+  /* OTHER */
+  { causeID: 35, causeName: "Non-Medical Science & Technology Research" },
+  { causeID: 42, causeName: "United Ways" },
+  { causeID: 43, causeName: "Jewish Federations" },
+  {
+    causeID: 44,
+    causeName: "Rescue Missions",
+  },
+];
