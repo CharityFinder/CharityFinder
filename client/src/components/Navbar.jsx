63d1cdd78bd3fa33e0ef98f7798a537dc09fcb5d
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-05-09 16:12:48 -0400

    updated nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,52 +1,21 @@
-import { Navbar as BootstrapNavbar, Nav, Image } from "react-bootstrap";
+import { Navbar as BootstrapNavbar, Nav } from "react-bootstrap";
 import React, { useContext } from "react";
 import { UserContext } from "../utils/auth";
-import cfLogo from "../images/charityfinderlogo.png";
-
-// TODO: Handle Navigation B/t components better
-// TODO: add color changing on phone dropdowns
-// TODO: brighten up the color for navbar links
+import { APP_NAME } from "../utils/constants";
 
 export const Navbar = ({ logoutHandler }) => {
   const { user } = useContext(UserContext);
 
   return (
-    <BootstrapNavbar
-      sticky="top"
-      expand="lg"
-      className="navbar-cf navbar-dark"
-      style={{ margin: 0, boxShadow: "#d2f9f0 2px 2px 6px" }}
-    >
-      <BootstrapNavbar.Brand href="/">
-        <Nav style={{ marginRight: 170 }}>
-          <Image
-            src={cfLogo}
-            width={200}
-            height={53}
-            style={{
-              position: "absolute",
-              top: 2,
-              left: 5,
-              paddingBottom: "1px",
-            }}
-          />
+    <BootstrapNavbar sticky="top" expand="lg" className="navbar-cf p-0">
+      <BootstrapNavbar.Brand className="p-0" href="/">
+        <Nav>
+          <h1 className="charity-finder ml-3 my-0">{APP_NAME}</h1>
         </Nav>
       </BootstrapNavbar.Brand>
-      <BootstrapNavbar.Toggle
-        className="toggle-cf"
-        aria-controls="basic-navbar-nav"
-      />
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
       <BootstrapNavbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link className="inactive" href="/">
-            Home
-          </Nav.Link>
-          <Nav.Link className="inactive" href="/about">
-            About
-          </Nav.Link>
-        </Nav>
-
-        <Nav className="ml-auto">
+        <Nav className="ml-auto mr-3">
           {!user ? (
             <>
               <Nav.Link href="/login">Login</Nav.Link>
@@ -62,6 +31,7 @@ export const Navbar = ({ logoutHandler }) => {
               </Nav.Link>
             </>
           )}
+          <Nav.Link href="/about">About</Nav.Link>
         </Nav>
       </BootstrapNavbar.Collapse>
     </BootstrapNavbar>
