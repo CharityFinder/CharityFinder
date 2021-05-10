24ab62e9f99ee10b65c6fda84177ab943be0c284
xshi0603 <xshi603@gmail.com>
2021-05-09 20:58:40 -0400

    added boilerplate code for the profiles page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,31 @@
+import React, { useState, useEffect, useContext } from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../utils/auth";
+import { CAUSES } from "../utils/constants";
 
 export const Profile = () => {
+
+  const { user, userData, interests } = useContext(UserContext);
+  
+  const generateInterests = () => {
+    return interests.map((interest) => {
+      return <p key={interest.causeId}>{CAUSES.filter(cause => cause.causeId == interest.causeId)[0].causeName}</p>
+    })
+  }
+
   return (
     <Container>
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
-      <p>This is profile page.</p>
-      <Link to={`/survey/`}>Survey Page</Link>
+      <p>Name: {userData.firstName} {userData.lastName} </p>
+      <p>Email: {userData.email} </p>
+      <p>City: {userData.city} </p>
+      <p>State: {userData.state} </p>
+      <p>Zipcode: {userData.zipcode}</p>
+      <br/>
+      <h3>Current Interests:</h3>
+      {generateInterests()}
+      <Link to={`/survey/`}>Retake Survey</Link>
     </Container>
   );
 };
