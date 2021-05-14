be904715e609ab0edb005ffd35314bb66f8ef490
xshi0603 <xshi603@gmail.com>
2021-05-14 17:40:18 -0400

    added adding/deletion feature for donations

--- client/src/pages/Donations.jsx
+++ client/src/pages/Donations.jsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Organization } from "../components/Organization";
-import { Container, Row, Table, Form, Button } from "react-bootstrap";
+import { Container, Table, Form, Button } from "react-bootstrap";
 import { UserContext } from "../utils/auth";
 
 export const Donations = () => {
   const { user } = useContext(UserContext);
-  const [contributions, setContributions] = useState({});
+  const [donationData, setDonationData] = useState({ name: "", amount: 0 });
+  const [errors, setErrors] = useState({});
+  const [contributions, setContributions] = useState([]);
 
+  const refreshDonations = async () => {
+    const res = await axios.get("/api/donations", {
+      params: {
+        userId: user.uid,
+      },
+    });
+    setContributions(res.data);
+  };
+
+  //get all donations that the user currently has
   useEffect(() => {
     const getDonations = async () => {
       const res = await axios.get("/api/donations", {
@@ -15,49 +26,90 @@ export const Donations = () => {
           userId: user.uid,
         },
       });
-      console.log(res.data);
-      // setContributions(res.data);
+      setContributions(res.data);
     };
 
     getDonations();
   }, [user]);
 
-  const addContributions = () => {};
+  const handleChange = (e) => {
+    setDonationData({
+      ...donationData,
+      [e.target.name]: e.target.value,
+    });
+    // Check and see if errors exist, and remove them from the error object:
+    if (!!errors[e.target.name])
+      setErrors({
+        ...errors,
+        [e.target.name]: null,
+      });
+  };
+
+  //should make an axios call to add a donation to the user
+  const handleSubmit = async (e) => {
+    await axios.post("/api/donations", null, {
+      params: {
+        orgName: donationData.name,
+        donationAmount: donationData.amount,
+        userId: user.uid,
+      },
+    });
+  };
+
+  //pulls id from svg tag and then refreshes donations 
+  const handleDelete = async (e) => {
+    await axios.delete("/api/donations/" + e.target.id, {
+      params: {
+        donationId: e.target.id,
+      },
+    });
+    refreshDonations();
+  };
+
+
 
   const generateContributions = () => {
-    let temp = [];
-    for (const [key, value] of Object.entries(contributions)) {
-      temp.push(
-        <tr>
-          <td>{key}</td>
-          <td>{value}</td>
+    return contributions.map((contribution) => {
+      return (
+        <tr key={contribution.id}>
+          <td>{contribution.orgName}</td>
+          <td>${contribution.donationAmount}</td>
+          <td style={{border:"1px solid white", backgroundColor: "white", width: "2rem"}}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16" id={contribution.id} onClick={handleDelete} style={{cursor:"pointer"}}>
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" id={contribution.id}/>
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" id={contribution.id}/>
+            </svg>
+          </td>
         </tr>
       );
-      console.log(key, value);
-    }
-    return temp;
+    });
   };
 
   return (
     <Container>
-      <Form>
-        <Form.Group controlId="formGroupEmail" >
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" style={{border:"1px solid #ced4da", borderRadius:".25rem"}}/>
+      <h2
+        className="mx-auto"
+        style={{ width: "50%", marginTop: "5%" }}
+      >
+        Add Donations:
+      </h2>
+      <Form onSubmit={handleSubmit} className="mx-auto" style={{width: "31.8vw", minWidth: "250px"}}>
+        <Form.Group controlId="formGroupOrganization" >
+          <Form.Label>Organization Name</Form.Label>
+          <Form.Control type="text" name="name" placeholder="Enter Organization Name" style={{border:"1px solid #ced4da", borderRadius:".25rem"}} onChange={handleChange}/>
         </Form.Group>
-        <Form.Group controlId="formGroupPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" style={{border:"1px solid #ced4da", borderRadius:".25rem"}}/>
+        <Form.Group controlId="formGroupDonation">
+          <Form.Label>Amount Donated</Form.Label>
+          <Form.Control type="number" name="amount" placeholder="Enter Amount Donated" style={{border:"1px solid #ced4da", borderRadius:".25rem"}} onChange={handleChange}/>
         </Form.Group>
         <Button variant="primary" type="submit">
             Submit
         </Button>
       </Form>
 
-
       <h2
         className="mx-auto"
-        style={{ width: "50%", paddingTop: 60, marginTop: "5%" }}
+        style={{ width: "50%", marginTop: "5%" }}
       >
         My Donations:
       </h2>
