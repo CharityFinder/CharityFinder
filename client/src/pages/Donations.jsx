15976d58bc89ff0089ce8774d238896c29fd1784
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-05-14 19:49:55 -0400

    removed duplicate api/donations call and added useCallback to remove the warning

--- client/src/pages/Donations.jsx
+++ client/src/pages/Donations.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import axios from "axios";
 import { Container, Table, Form, Button } from "react-bootstrap";
 import { UserContext } from "../utils/auth";
@@ -9,28 +9,19 @@ export const Donations = () => {
   const [errors, setErrors] = useState({});
   const [contributions, setContributions] = useState([]);
 
-  const refreshDonations = async () => {
+  const refreshDonations = useCallback(async () => {
     const res = await axios.get("/api/donations", {
       params: {
         userId: user.uid,
       },
     });
     setContributions(res.data);
-  };
+  }, [user.uid]);
 
   //get all donations that the user currently has
   useEffect(() => {
-    const getDonations = async () => {
-      const res = await axios.get("/api/donations", {
-        params: {
-          userId: user.uid,
-        },
-      });
-      setContributions(res.data);
-    };
-
-    getDonations();
-  }, [user]);
+    refreshDonations();
+  }, [user, refreshDonations]);
 
   // finds form errors
   const findFormErrors = () => {
@@ -39,7 +30,8 @@ export const Donations = () => {
     // name errors
     if (name === "") newErrors.name = "cannot be blank!";
     // donation errors
-    if (amount === 0 || amount === NaN || amount === "") newErrors.amount = "cannot be blank!";
+    if (amount === 0 || amount === NaN || amount === "")
+      newErrors.amount = "cannot be blank!";
 
     return newErrors;
   };
@@ -77,7 +69,7 @@ export const Donations = () => {
     }
   };
 
-  //pulls id from svg tag and then refreshes donations 
+  //pulls id from svg tag and then refreshes donations
   const handleDelete = async (e) => {
     await axios.delete("/api/donations/" + e.target.id, {
       params: {
@@ -94,7 +86,7 @@ export const Donations = () => {
       retVal += parseInt(contribution.donationAmount);
     });
     return retVal;
-  }
+  };
 
   const generateContributions = () => {
     return contributions.map((contribution) => {
@@ -102,10 +94,32 @@ export const Donations = () => {
         <tr key={contribution.id}>
           <td>{contribution.orgName}</td>
           <td>${contribution.donationAmount}</td>
-          <td style={{border:"1px solid white", backgroundColor: "white", width: "2rem"}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16" id={contribution.id} onClick={handleDelete} style={{cursor:"pointer"}}>
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" id={contribution.id}/>
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" id={contribution.id}/>
+          <td
+            style={{
+              border: "1px solid white",
+              backgroundColor: "white",
+              width: "2rem",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-x-circle"
+              viewBox="0 0 16 16"
+              id={contribution.id}
+              onClick={handleDelete}
+              style={{ cursor: "pointer" }}
+            >
+              <path
+                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                id={contribution.id}
+              />
+              <path
+                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+                id={contribution.id}
+              />
             </svg>
           </td>
         </tr>
@@ -115,30 +129,41 @@ export const Donations = () => {
 
   return (
     <Container>
-      <h2
-        className="mx-auto"
-        style={{ width: "50%", marginTop: "5%" }}
-      >
+      <h2 className="mx-auto" style={{ width: "50%", marginTop: "5%" }}>
         Add Donations:
       </h2>
-      <Form onSubmit={handleSubmit} className="mx-auto" style={{width: "31.8vw", minWidth: "250px"}}>
-        <Form.Group controlId="formGroupOrganization" >
+      <Form
+        onSubmit={handleSubmit}
+        className="mx-auto"
+        style={{ width: "31.8vw", minWidth: "250px" }}
+      >
+        <Form.Group controlId="formGroupOrganization">
           <Form.Label>Organization Name</Form.Label>
-          <Form.Control type="text" name="name" placeholder="Enter Organization Name" style={{border:"1px solid #ced4da", borderRadius:".25rem"}} onChange={handleChange} isInvalid={!!errors.name}/>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter Organization Name"
+            style={{ border: "1px solid #ced4da", borderRadius: ".25rem" }}
+            onChange={handleChange}
+            isInvalid={!!errors.name}
+          />
         </Form.Group>
         <Form.Group controlId="formGroupDonation">
           <Form.Label>Amount Donated</Form.Label>
-          <Form.Control type="number" name="amount" placeholder="Enter Amount Donated" style={{border:"1px solid #ced4da", borderRadius:".25rem"}} onChange={handleChange} isInvalid={!!errors.amount}/>
+          <Form.Control
+            type="number"
+            name="amount"
+            placeholder="Enter Amount Donated"
+            style={{ border: "1px solid #ced4da", borderRadius: ".25rem" }}
+            onChange={handleChange}
+            isInvalid={!!errors.amount}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
-            Submit
+          Submit
         </Button>
       </Form>
-
-      <h2
-        className="mx-auto"
-        style={{ width: "50%", marginTop: "5%" }}
-      >
+      <h2 className="mx-auto" style={{ width: "50%", marginTop: "5%" }}>
         My Donations:
       </h2>
       Total Contributions: ${getTotalContributions()}
