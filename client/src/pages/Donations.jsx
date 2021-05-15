0265ae3e6cd7741e3354e372ae9692a5ed33e543
xshi0603 <xshi603@gmail.com>
2021-05-15 16:26:46 -0400

    added functionality to hide the donation id from users

--- client/src/pages/Donations.jsx
+++ client/src/pages/Donations.jsx
@@ -70,10 +70,10 @@ export const Donations = () => {
   };
 
   //pulls id from svg tag and then refreshes donations
-  const handleDelete = async (e) => {
-    await axios.delete("/api/donations/" + e.target.id, {
+  const handleDelete = async (id) => {
+    await axios.delete("/api/donations/" + id, {
       params: {
-        donationId: e.target.id,
+        donationId: id,
       },
     });
     refreshDonations();
@@ -108,17 +108,16 @@ export const Donations = () => {
               fill="currentColor"
               className="bi bi-x-circle"
               viewBox="0 0 16 16"
-              id={contribution.id}
-              onClick={handleDelete}
+              onClick={() => {
+                handleDelete(contribution.id)
+              }}
               style={{ cursor: "pointer" }}
             >
               <path
                 d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                id={contribution.id}
               />
               <path
                 d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
-                id={contribution.id}
               />
             </svg>
           </td>
