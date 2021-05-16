5110ac5bd6dac4f6ad1871e541c70dd4ac5ec697
sdhani <shania.dhani46@myhunter.cuny.edu>
2021-05-15 21:25:33 -0400

    included star ratings and css updatez

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Card as BootstrapCard } from "react-bootstrap";
 import { Favorite } from "../components/Favorite";
 import "../styles/BigCard.css";
+import { FiveStars } from "./FiveStars";
+import { ArrowUpRightCircleFill } from "react-bootstrap-icons";
+import { Ratings } from "./Ratings";
 
 export const Card = ({ name, ein, isFavorited, organization }) => {
   const [mailingAddress, setMailingAddress] = useState({
@@ -10,6 +13,7 @@ export const Card = ({ name, ein, isFavorited, organization }) => {
     postalCode: "",
   }); //used so that you don't try to render data before it is loaded
 
+  console.log("BLOB", organization);
   useEffect(() => {
     if (organization.length !== 0) {
       setMailingAddress({
@@ -23,11 +27,21 @@ export const Card = ({ name, ein, isFavorited, organization }) => {
   return (
     <BootstrapCard className="column bigcard" style={{ margin: 5 }}>
       <BootstrapCard.Body>
-        <BootstrapCard.Title>{name}</BootstrapCard.Title>
+        <BootstrapCard.Title>
+          {name}{" "}
+          <a
+            href={organization.websiteURL}
+            rel="noreferrer"
+            target="_blank"
+            title="Visit their website"
+          >
+            <ArrowUpRightCircleFill />
+          </a>
+        </BootstrapCard.Title>
         <BootstrapCard.Text>
           {organization.tagLine} <br />
           <br />
-          {organization.mission} <br />
+          <br />
           <br />
           {mailingAddress["city"] +
             ", " +
@@ -43,19 +57,148 @@ export const Card = ({ name, ein, isFavorited, organization }) => {
           isFavorited={isFavorited}
           organization={organization}
         />
-        <a
-          href={organization.websiteURL}
-          rel="noreferrer"
-          target="_blank"
-          style={{
-            textAlign: "center",
-            position: "absolute",
-            bottom: 25,
-            left: 25,
-          }}
-        >
-          Website Here
-        </a>
+
+        {organization && organization.category && organization.cause ? (
+          <>
+            {organization.category.image && (
+              <img src={organization.category.image} alt="category" />
+            )}
+            {organization.cause.image && (
+              <img src={organization.cause.image} alt="cause" />
+            )}
+            {organization.category.categoryName && (
+              <p>Category Name: {organization.category.categoryName}</p>
+            )}
+            {organization.cause.causeName && (
+              <p>Cause Name: {organization.cause.causeName}</p>
+            )}
+            {organization.activeAdvisories._rapid_links.related && (
+              // TODO: ACCOUNT FOR MULTIPLE ACTIVE ADVISORIES
+              <div className="bg-red">
+                <a
+                  href={organization.activeAdvisories._rapid_links.related}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  RED ACTIVE ADVISORY
+                </a>
+              </div>
+            )}
+            {organization.removedAdvisories._rapid_links.related && (
+              // TODO: ACCOUNT FOR MULTIPLE ACTIVE ADVISORIES
+              <div className="bg-red">
+                <a
+                  href={organization.activeAdvisories._rapid_links.related}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  PAST ADVISORY
+                </a>
+              </div>
+            )}
+            <Ratings
+              ratings={[
+                {
+                  title: "Accountability",
+                  rating:
+                    organization.currentRating.accountabilityRating.rating,
+                },
+                {
+                  title: "Financial",
+                  rating: organization.currentRating.financialRating.rating,
+                },
+              ]}
+            />
+            {organization.mission}
+            {organization.charityNavigatorURL && (
+              <a
+                href={organization.charityNavigatorURL}
+                rel="noreferrer"
+                target="_blank"
+              >
+                Find out more about ratings on Charity Navigator
+              </a>
+            )}
+            {/* TODO: FIX RATING IMAGE PROBLEM */}
+            {organization.ratingImage && organization.ratingImage.small && (
+              <img src={organization.ratingImage.small} alt="cnRatingimg" />
+            )}
+
+            {organization.irsClassification && (
+              <div className="irs-info">
+                IRS CLASSIFICATION STUFF{" "}
+                {Object.entries(organization.irsClassification).map(
+                  ([key, value]) => {
+                    const name = key
+                      .replace(/([a-z])([A-Z])/g, "$1 $2")
+                      .replace(/ntee/, "NTEE");
+                    return value ? (
+                      <p>
+                        <span style={{ textTransform: "capitalize" }}>
+                          {name}:
+                        </span>{" "}
+                        {value}
+                      </p>
+                    ) : null;
+                  }
+                )}
+                IT BEGGGINNNNS
+                {organization.irsClassification.accountingPeriod && (
+                  <p>
+                    Accounting Period:{" "}
+                    {organization.irsClassification.accountingPeriod}
+                  </p>
+                )}
+                {organization.irsClassification.subsection && (
+                  <p>Subsection: {organization.irsClassification.subsection}</p>
+                )}
+                {/* TODO: DISPLAY AS MONETARY VALUE */}
+                {organization.irsClassification.incomeAmount && (
+                  <p>
+                    Income Amount: {organization.irsClassification.incomeAmount}
+                  </p>
+                )}
+                {organization.irsClassification.deductibility && (
+                  <p>
+                    Deductibility:{" "}
+                    {organization.irsClassification.deductibility}
+                  </p>
+                )}
+                {organization.irsClassification.exemptOrgStatus && (
+                  <p>
+                    Exempt Organization Status:{" "}
+                    {organization.irsClassification.exemptOrgStatus}
+                  </p>
+                )}
+                {organization.irsClassification.filingRequirement && (
+                  <p>
+                    Filing Requirement:{" "}
+                    {organization.irsClassification.filingRequirement}
+                  </p>
+                )}
+                {organization.irsClassification.foundationStatus && (
+                  <p>
+                    Foundation Status:{" "}
+                    {organization.irsClassification.foundationStatus}
+                  </p>
+                )}
+                {organization.irsClassification.affiliation && (
+                  <p>
+                    Affiliation: {organization.irsClassification.affiliation}
+                  </p>
+                )}
+              </div>
+            )}
+            {organization.mailingAddress.streetAddress1 && (
+              <p>
+                Street Address: {organization.mailingAddress.streetAddress1}
+              </p>
+            )}
+            {organization.mailingAddress.postalCode && (
+              <p>Postal Code: {organization.mailingAddress.postalCode}</p>
+            )}
+          </>
+        ) : null}
       </BootstrapCard.Body>
     </BootstrapCard>
   );
